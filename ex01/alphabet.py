db2ac4cbd99ce1a635dd5d27f698e68e16a26437
c0a21015 <c0a210154a@edu.teu.ac.jp>
2022-11-22 17:57:11 +0900

    アルファベット探し

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,30 +1,22 @@
 import random
 import time
 
-#グローバル変数定義
-num_of_alphabet = 26 #全アルファベット数
-num_of_all_chars = 10 #対象文字数
-num_of_abs_chars = 2 #欠損文字数
-num_of_trials = 2 #最大繰り返し回数
-
 def shutudai(alphabet):
-    #全アルファベットから対象文字を10個選択する。(重複なし)
-    all_chars = random.sample(alphabet, num_of_all_chars)
+    
+    all_chars = random.sample(alphabet,10)
     print("対象文字：")
     for c in all_chars:
         print(c, end=" ")
     print()
 
-    #対象文字から欠損文字を2個選択する。(重複なし)
-    abs_chars = random.sample(all_chars, num_of_abs_chars)
-    print("欠損文字(デバッグ用)：")
+    abs_chars = random.sample(all_chars, 2)
+    print("欠損文字：")
     for c in abs_chars:
         print(c, end=" ")
     print()
 
     print("表示文字：")
     for c in all_chars:
-        #abs_chars に含まれていなければ表示文字として表示する
         if c not in abs_chars:
            print(c, end=" ")
     print()
@@ -34,7 +26,7 @@ def shutudai(alphabet):
 
 def kaitou(abs_chars):
     num = int(input("欠損文字はいくつあるでしょうか？："))
-    if num != num_of_abs_chars:
+    if num != 2:
         print("不正解です。")
     else:
         print("正解です。それでは、具体的に欠損文字を1つずつ入力してください。")
@@ -52,9 +44,9 @@ def kaitou(abs_chars):
 
 if __name__ == "__main__":
     st = time.time()
-    alphabet = [chr(i + 65) for i in range(num_of_alphabet)]
+    alphabet = [chr(i + 65) for i in range(26)]
     print(alphabet)
-    for _ in range(num_of_trials):
+    for _ in range(2):
         abs_chars = shutudai(alphabet)
         ret = kaitou(abs_chars)
         if ret:
