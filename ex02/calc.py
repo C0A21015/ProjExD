b3b95c7bc6d4934891e6c707ac27227a557878fc
c0a21015 <c0a210154a@edu.teu.ac.jp>
2022-11-29 16:53:43 +0900

    電卓

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,41 +1,91 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+import math
 
 def button_click(event):
     btn = event.widget
     num = btn["text"]
+    siki = entry.get() # 数式の文字列
     if num == "=":
-        siki = entry.get() # 数式の文字列
-        res = eval(siki) # 数式文字列の評価
-        entry.delete(0, tk.END) # 表示文字列の削除
-        entry.insert(tk.END, res) # 結果の挿入
+        if siki=="":
+            pass
+        else:
+            if siki[-1]=="+" or siki[-1]=="-" or siki[-1]=="*" or siki[-1]=="/"or siki[-1]==".":
+                pass
+            else:        
+                res = eval(siki) # 数式文字列の評価
+                entry.delete(0, tk.END) # 表示文字列の削除
+                entry.insert(tk.END, res) # 結果の挿入
+    elif num=="+" :
+        if siki == "":
+            pass
+        else:
+            if siki[-1]=="+" or siki[-1]=="*" or siki[-1]=="/" or siki[-1]=="." :
+                pass
+            else:
+                entry.insert(tk.END,num)
+
+    elif num=="-" :
+        if siki == "":
+            entry.insert(tk.END,num)
+        else:
+            if siki[-1]=="." :
+                pass
+            else:
+                entry.insert(tk.END,num)            
+
+    elif num==".":
+        if siki == "":
+            pass
+        else:
+            if siki[-1]=="+" or siki[-1]=="-" or siki[-1]=="*" or siki[-1]=="/" or siki[-1]=="." :
+                pass
+            else:
+                entry.insert(tk.END,num)            
+    elif num == "×":
+        if siki=="":
+            pass
+        else:
+            if siki[-1]=="+" or siki[-1]=="-"or siki[-1]=="*" or siki[-1]=="/"or siki[-1]==".":
+                pass
+            else:
+                entry.insert(tk.END,"*")
+    elif num == "÷":
+        if siki == "":
+            pass
+        else:
+            if siki[-1]=="+" or siki[-1]=="-" or siki[-1]=="*" or siki[-1]=="/"or siki[-1]=="." :
+                pass
+            else:
+                entry.insert(tk.END,"/")
+
+    elif num == "C":
+        entry.delete(0,tk.END)   
+
+    elif num == "π":
+        entry.insert(tk.END,f"{math.pi}")
+
+    elif num == "e":
+        entry.insert(tk.END,f"{math.e}")    
+
     else: # 「=」以外のボタン字
         #tkm.showinfo("", f"{num}ボタンがクリックされました")
         entry.insert(tk.END, num)
 
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("450x600")
 
-entry = tk.Entry(root, justify="right", width=10, font=("",40))
-entry.grid(row=0, column=0, columnspan=3)
+entry = tk.Entry(root, justify="right", width=15, font=("",40))
+entry.grid(row=0, column=0, columnspan=4)
 
 r, c = 1, 0
-for num in range(9, -1, -1):
-    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
-    button.grid(row=r, column=c)
-    button.bind("<1>", button_click)
-    c += 1
-    if c%3 == 0:
-        r += 1
-        c = 0
-
-operators = ["+", "="]
+operators = ["00","e","π","+","7","8","9","-","4","5","6","×","1","2","3","÷","C","0",".", "="]
 for ope in operators:
     button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
     button.bind("<1>", button_click)
     c += 1
-    if c%3 == 0:
+    if c%4 == 0:
         r += 1
         c = 0
 
