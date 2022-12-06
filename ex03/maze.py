a897e794ec9669f9369b8404fb96bf6dda9b3913
c0a21015 <c0a210154a@edu.teu.ac.jp>
2022-12-06 14:57:17 +0900

    迷路

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import maze_maker as mm
 
 def key_down(event):
     global key
@@ -11,23 +12,27 @@ def key_up(event):
 def main_proc():
     global cx , cy
     if key == "Up":
-        cy -=20
+        cy -=100
     elif key == "Down":
-        cy += 20
+        cy += 100
     elif key == "Left":
-        cx -= 20
+        cx -= 100
     elif key == "Right":
-        cx += 20
+        cx += 100
     canvas.coords("kokaton" , cx , cy) 
-    root.after(100 , main_proc)          
+    root.after(150 , main_proc)          
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root , width=1500 , height=900 , bg="black")
     canvas.pack()
+
+    maze_list = mm.make_maze(15,9)
+    mm.show_maze(canvas , maze_list)
+
     images = tk.PhotoImage(file="fig/0.png")
-    cx , cy = 300 , 400
+    cx , cy = 150 , 150 #初期位置
     canvas.create_image(cx , cy , image=images , tag="kokaton")
     key=""
     root.bind("<KeyPress>" , key_down)
