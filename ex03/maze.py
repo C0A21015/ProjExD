55157d3a31de0ffe7841bd981002de19022688dd
c0a21015 <c0a210154a@edu.teu.ac.jp>
2022-12-06 16:56:08 +0900

    迷路

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import maze_maker as mm
+import tkinter.messagebox as tkm
+import random
 
 def key_down(event):
     global key
@@ -13,7 +15,7 @@ def main_proc():
     global cx , cy , mx , my
     if key == "Up":
         my -=1
-    elif key == "Down":
+    elif key == "Down": 
         my += 1
     elif key == "Left":
         mx -= 1
@@ -21,33 +23,45 @@ def main_proc():
         mx += 1
     if maze_list[mx][my] == 1: #移動先が壁なら
         if key == "Up":
-           my +=1
+            my +=1
         elif key == "Down":
             my -= 1
         elif key == "Left":
             mx += 1
         elif key == "Right":
             mx -= 1
+
     cx , cy = mx*100+50 , my*100+50     
     canvas.coords("kokaton" , cx , cy) 
     root.after(100 , main_proc)          
+
+def option():
+    global cx , cy, mx , my , key
+    if key == "r": #「ｒ」でリセット
+        mx , my = 1  , 0
+    if mx == 13 and my ==8:  #ゴールしたことを知らせる
+        tkm.showinfo("ゴール","ゴールしました")
+    cx , cy = mx*100+50 , my*100+50
+    canvas.coords("kokaton" , cx , cy)  
+    root.after(100 , option)   
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root , width=1500 , height=900 , bg="black")
     canvas.pack()
-
     maze_list = mm.make_maze(15,9)
     mm.show_maze(canvas , maze_list)
-
-    images = tk.PhotoImage(file="fig/0.png")
-
-    mx , my = 1 , 1
+    num = random.randint(0,9)
+    images = tk.PhotoImage(file=f"fig/{num}.png")
+    mx , my = 1 , 0
     cx , cy = mx*150 , my*150 #初期位置
     canvas.create_image(cx , cy , image=images , tag="kokaton")
     key=""
     root.bind("<KeyPress>" , key_down)
     root.bind("<KeyRelease>" , key_up)
     main_proc()
+    option()
     root.mainloop()
