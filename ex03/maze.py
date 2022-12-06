916bd2389f40053b5db08225ae2eeaa97ac3c7ed
c0a21015 <c0a210154a@edu.teu.ac.jp>
2022-12-06 14:18:05 +0900

    迷路

---
+++ ex03/maze.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+    canvas = tk.Canvas(root , width=1500 , height=900 , bg="black")
+    canvas.pack()
+    #root.geometry("1")
+    root.mainloop()
